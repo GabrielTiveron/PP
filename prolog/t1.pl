ae615e2e5b28736feb24fcb97c0d1edf39b5bcfb
Gabriel Tiveron <gabrielmtiveron@gmail.com>
2021-05-11 15:22:33 -0300

    Add output stream feature

--- prolog/t1.pl
+++ prolog/t1.pl
@@ -4,44 +4,62 @@
 
 main(Argv):-
   prompt(_,''),
-  % init_list(L),
-  % write(L),
-  read_file(Argv, L),
-  % write(L).
-  treat_lines(L).
+  treat_entry(Argv, In, Out),
+  %% write(In),nl,write(Out),nl,
+  read_file(In, L),
+  treat_lines(L,Out).
 
-init_list([]).
+treat_entry([X|[]], I, O):-
+  I = X, O = [].
+
+treat_entry([X,Y], I, O):-
+  I = X, O = Y,
+  open(O, write,Out),
+  write(Out, ""),
+  close(Out).
+
+init_list([]). % Inicializar lista vazia
 
 % --------------- Tratamento de linhas do arquivo --------------- %
-treat_lines([]).
+treat_lines([],_).
 
-treat_lines([X|[]]):-
+treat_lines([X|[]],[]):-
   write(X),nl.
 
-treat_lines([X,Y|T]):-
+treat_lines([X,Y|T],[]):-
   X \= Y,
   write(X),nl,
   append([Y],T,New),
-  treat_lines(New).
+  treat_lines(New,[]).
 
-treat_lines([_,Y|T]):-
+treat_lines([X|[]], N):-
+  open(N, append, Out),
+  write(Out, X), write(Out, "\n"),
+  close(Out).
+
+treat_lines([X,Y|T], N):-
+  X \= Y,
+  open(N, append, Out),
+  write(Out, X), write(Out, "\n"),
+  close(Out),
+  append([Y],T,New),
+  treat_lines(New,N).
+
+treat_lines([_,Y|T],N):-
   append([Y], T, New),
-  treat_lines(New).
+  treat_lines(New,N).
 
 % --------------- Fim Tratamento de linhas de Entrada --------------- %
 % --------------- Leitura de linhas de Entrada --------------- %
 
-read_file([N], L):-
+read_file(N, L):-             % Leitura de arquivo se fornecido
   read_file_to_string(N, X, []),
   split_string(X, "\n", "\n", L).
 
-read_file([], L):-
+read_file([], L):-                      % Leitura via entrada padrão. (Ex. cat)
   read_line_to_string(user_input, Line),
   Line \= end_of_file,
-  % append([], L, New),
-  write(Line),nl,
-  % write(New),
-  append([Line], L,New),
+  append([Line], New, L),
   read_file([],New).
 
 read_file([],_).
